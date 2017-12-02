e4d3b88780a3602b07044443e9349f84d155d49b
Colin Luzio <colinluzio@yahoo.co.uk>
2017-12-02 09:00:37 +0000

    show list of current opponents based on search result

--- src/containers/games_list.js
+++ src/containers/games_list.js
@@ -3,16 +3,18 @@ import {connect} from 'react-redux';
 
 class GamesList extends Component{
 
-    renderGames(gameData){
+    renderGames(user,gameData){
+        var white = gameData.white.substring(gameData.white.lastIndexOf("/") + 1);
+        var user  = (user == white ? gameData.black.substring(gameData.black.lastIndexOf("/") + 1) : white);
         return(
-            <tr>
-                <td>{gameData.white.substring(gameData.white.lastIndexOf("/") + 1)}</td>
+            <tr key={user}>
+                <td>{user}</td>
             </tr>
         )
     }
 
     render(){
-        console.log(this.props);
+
         return(
             <table className="table table-hover">
             <thead>
@@ -21,15 +23,15 @@ class GamesList extends Component{
                 </tr>
             </thead>
             <tbody>
-                {this.props.games.games[0].map(this.renderGames)}
+                {this.props.data.games[0].map(this.renderGames.bind(this, this.props.data.user))}
             </tbody>
             </table>
         )
     }
 }
 
-function mapStateToProps(games){
-    return {games};
+function mapStateToProps(data){
+    return {data};
 }
 
 export default connect(mapStateToProps)(GamesList);
