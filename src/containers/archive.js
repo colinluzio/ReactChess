dfda839d404443157d88a5b9b05117ec3c831c85
Colin Luzio <colinluzio@yahoo.co.uk>
2017-12-17 10:44:28 +0000

    prevent infinite loop being called

--- src/containers/archive.js
+++ src/containers/archive.js
@@ -2,52 +2,82 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fetchArchive} from '../actions/archive';
+import ReactPaginate from 'react-paginate';
 import { GetBestWins } from '../helpers/game_helpers'
 
 class Archive extends Component{
 
     constructor(props){
         super(props);
+
+        this.state = {
+            user: '',
+            activePage: 0,
+            wins : []
+        };
+    }
+    componentDidUpdate(){
+        let user = (this.props.data.user ? this.props.data.user[0].username : '');
+        let wins = (this.props.data.archive ? GetBestWins(this.props.data.archive[0],this.state.user) : []);
+        
+        if(wins != this.state.wins && user != this.state.user){
+            this.setState({wins: wins,user:user});
+        }
     }
     fetchArchive(user,event){
         event.preventDefault();
         this.props.fetchArchive(user);
     }
+    handlePageClick (data) {
+       //console.log(ReferralMain.props);
+       //console.log(this.props);
 
+
+   };
     render(){
-        var user = {};
-        var wins = [];
-        if(this.props.data.user){
-            user = this.props.data.user[0].username;
-        }
-        if(this.props.data.archive){
-            wins = GetBestWins(this.props.data.archive[0],user);
-        }
-        console.log(wins);
+
         return(
             <div>
                 <div className = "row">
                     <div className="col-xs-12">
-                        <button type="button" className="btn btn-primary" onClick={this.fetchArchive.bind(this, user)}>Fetch Archive</button>
+                        <button type="button" className="btn btn-primary" onClick={this.fetchArchive.bind(this, this.state.user)}>Fetch Archive</button>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-12">
-                    {wins.length > 0 ? (
-                        <table className="table table-striped">
-                           <thead>
-                             <tr>
-                               <th>User</th>
-                               <th>Rating</th>
-                               <th>Played as</th>
-                             </tr>
-                           </thead>
-                           <tbody>
-                               {wins.map(function(item,i){
-                                  return <tr key={i}><td>{item.opponent}</td><td>{item.rating}</td><td>{item.color}</td></tr>
-                               })}
-                           </tbody>
-                           </table>
+                    {this.state.wins.length > 0 ? (
+                        <div>
+                            <div>
+                                <table className="table table-striped">
+                                   <thead>
+                                     <tr>
+                                       <th>User</th>
+                                       <th>Rating</th>
+                                       <th>Played as</th>
+                                     </tr>
+                                   </thead>
+                                   <tbody>
+                                       {this.state.wins.map(function(item,i){
+                                          return <tr key={i}><td>{item.opponent}</td><td>{item.rating}</td><td>{item.color}</td></tr>
+                                       })}
+                                   </tbody>
+                                </table>
+                            </div>
+                            <div>
+                            <ReactPaginate previousLabel={"previous"}
+                                       nextLabel={"next"}
+                                       breakLabel={<a href="">...</a>}
+                                       breakClassName={"break-me"}
+                                       pageCount={20}
+                                       marginPagesDisplayed={2}
+                                       pageRangeDisplayed={5}
+                                       onPageChange={this.handlePageClick.bind(this)}
+                                       containerClassName={"pagination"}
+                                       subContainerClassName={"pages pagination"}
+                                       activeClassName={"active"} />
+                            </div>
+                        </div>
+
                 ) : (<div>No wins for that user!</div>)}
                     </div>
                 </div>
