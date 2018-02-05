449c2818ef02435f98ae559d249d6c4af4239530
Colin Luzio <colinluzio@yahoo.co.uk>
2018-02-05 20:16:24 +0000

    add authentication HOC

---
+++ src/components/require_authentication.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export default function (ComposedComponent){
+    class Authentication extends Component{
+        static contextTypes = {
+            router: React.PropTypes.object
+        }
+
+        componentWillMount(){
+            this.context.router.push('/');
+        }
+        render(){
+            return <ComposedComponent {...this.props}/>
+        }
+    }
+
+
+function mapStateToProps(state){
+    return {authenticated: state.authenticated};
+}
+
+    return connect(mapStateToProps)(Authentication);
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import{Router, Route, browserHistory} from 'react-router';
 import Resources from './components/resources';
+import requireAuth from './components/require_authentication';
 import Form from './components/form';
 import App from './components/app';
 import reducers from './reducers';
@@ -15,7 +16,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <Route path ="/resources" component={Resources}/>
+            <Route path ="/resources" component={requireAuth(Resources)}/>
             <Route path ="/login" component={Form}/>
         </Route>
     </Router>
